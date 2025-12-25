fb099961744f7d67b712c8a200dd8d41db5f7dcb
Waqas <waqaskhan1437@gmail.com>
2025-12-25 15:20:46 +0500

    kfjdslfjl

--- src/modules/page-builder/frontend/scripts/script-utils.js
+++
@@ -1,1 +0,0 @@
-export const END_SCRIPT = String.fromCharCode(60, 47, 115, 99, 114, 105, 112, 116, 62);
--- src/modules/products/frontend/scripts/product-form.js
+++
@@ -1,6 +0,0 @@
-/**
- * Product Form Loader
- * Original 557-line file has been split into modules in /product-form/ folder
- */
-
-import './product-form/main.js';
